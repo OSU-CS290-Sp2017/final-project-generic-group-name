240e227626fc319ac549ee7e87a879d33d66b42a
DanielDomme <dommed@oregonstate.edu>
2017-06-07 12:16:51 -0700

    mostly working server code

--- server.js
+++ server.js
@@ -4,25 +4,59 @@ var exphbs = require('express-handlebars');
 var fs = require('fs');
 var app = express();
 var port = process.env.PORT || 3000;
+var members = require('./members');
 
-/*
-//add members json file
-var membersData = require('./')
-*/
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-/*
-app.get('/', function(req, res, next){
-  var templateArgs={
-      //add our templateArgs
-  }
-  res.status(200).render('', templateArgs);
-});
-*/
+app.get('/', function (req, res, next) {
+	var tempTitle = "Women's Water Polo Club";
+  var templateArgs = {
+    title: tempTitle
+	
+  };
 
-//add individual member pages here
+  res.render('index', templateArgs);
+
+});
+app.get('/index.html', function (req, res, next) {
+	var tempTitle = "Women's Water Polo Club";
+  var templateArgs = {
+    title: tempTitle
+	
+  };
+
+  res.render('index', templateArgs);
+
+});
+
+app.get('/calendar.html', function (req, res, next){
+	var tempTitle = "June 2017 Events Calendar";
+	var templateArgs = {
+		title: tempTitle
+	}
+	res.render('calendar', templateArgs);
+});
+app.get('/calendar', function (req, res, next){
+	var tempTitle = "June 2017 Events Calendar";
+	var templateArgs = {
+		title: tempTitle
+	}
+	res.render('calendar', templateArgs);
+});
+app.get('/aboutMembers.html', function(req,res,next){
+	var tempTitle = "Women's Water Polo Club Members";
+	var templateArgs = {
+		people: members,
+		title: tempTitle
+	}
+	res.render('aboutMembers', templateArgs);
+});
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'slideshow_images')));
 
 app.get('*', function (req, res) {
   res.status(404).render('404Page');
