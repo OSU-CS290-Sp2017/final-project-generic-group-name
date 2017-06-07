6950b1db8247cacc5ff572c2f6f111cddddfbd3b
weirbr <weirbr@oregonstate.edu>
2017-06-06 20:22:03 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -5,13 +5,22 @@ var fs = require('fs');
 var app = express();
 var port = process.env.PORT || 3000;
 
+/*
+//add members json file
+var membersData = require('./')
+*/
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-
-
-
+/*
+app.get('/', function(req, res, next){
+  var templateArgs={
+      //add our templateArgs
+  }
+  res.status(200).render('', templateArgs);
+});
+*/
 
 
 app.get('*', function (req, res) {
