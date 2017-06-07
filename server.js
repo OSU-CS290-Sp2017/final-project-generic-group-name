2ff929a79f1916660d65297b7e48e65b8f5c893a
Daniel Domme <Daniel Domme>
2017-06-06 18:21:46 -0700

    added server code

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var fs = require('fs');
+var app = express();
+var port = process.env.PORT || 3000;
 
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+
+
+
+
+app.get('*', function (req, res) {
+  res.status(404).render('404Page');
+});
+
+app.listen(port, function () {
+  console.log("== Server listening on port", port);
+});
